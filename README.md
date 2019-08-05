7182f6fef2f317f8782b655ca58dbfd9d801f412
Antti Matikainen <antti.o.matikainen@aalto.fi>
2019-08-05 18:46:12 +0300

    Adding functionality. Pulling metadata from DigiTrafi in order to get station ID numbers.

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # ilmatieteenlaitosAvoinData
-# This is a small software development project intended to create
-# a simple piece of software with a GUI that could be used to
-# select a weather station (or stations) and to pull data from it
-# for a selected time period.
-#
-# This is to be done using the Finnish Meterological Center open API.
-#
-# Should this project ever be finished, it will be published under the
-# MIT License.
+This is a small software development project intended to create a simple piece of software with a GUI that could be used to select a weather station (or stations) and to save data from them over a selected time period into a CSV file.
+
+This is to be done using the several open data APIs including the Finnish Meterological Institute.
+
+Should this project ever be finished, it will be published under the MIT License.
+
+The API used publishes data under the Creative Commons license.
--- apiStationList.py
+++ apiStationList.py
@@ -1,1 +1,46 @@
+# File: apiSTationList.py
+# Contains functions:
+# * returnStationList()
+# Purpose of function:
+# To pull up an up to date list of weather stations and road weather stations so that the user may select the relevant ones. 
+# Inputs: none
+# Outputs: List of Weather Stations containing:
+#  * Station name in English
+#  * Station municipality
+#  * Station province
+#  * Station ID
 
+import requests
+import json
+# Road station IDs and such can be pulled from the digitraffic API weather sensor metadata.
+#https://tie.digitraffic.fi/api/v1/metadata/weather-stations
+# Information for a specific sensor can be found from
+#https://tie.digitraffic.fi/api/v1/data/weather-data/ID
+# The information is in JSON format.
+
+def returnStationList():
+  # Create an empty list to append tuples containing the relevant information.
+  stationList = []
+  # Retrieve the metadata for road weather sensors.
+  roadMetaDataResponse = requests.get("https://tie.digitraffic.fi/api/v1/metadata/weather-stations")
+
+  # Check if the GET request is successfull. Return false otherwise.
+  if (roadMetaDataResponse.status_code == 200):
+    # Parse the JSON data
+    parsedMetaData = json.loads(roadMetaDataResponse.text)
+    for entry in parsedMetaData["features"]:
+      # Append the tuple to the list.
+      # Use try/except in order to catch malformed entries to catch bad entries
+      try:
+        stationList.append((entry["properties"]["names"]["en"],entry["properties"]["municipality"],entry["properties"]["province"],entry["properties"]["roadStationId"]))
+      except KeyError:
+        stationList.append((entry["properties"]["name"],entry["properties"]["municipality"],entry["properties"]["province"],entry["properties"]["roadStationId"]))
+
+  elif (roadMetaDataResponse.status_code == 404):
+    return false
+
+
+  # Return the list.
+  return stationList
+
+print(len(returnStationList()))
